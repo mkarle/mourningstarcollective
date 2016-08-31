df62482f1646e38ee854b61dbd5c471b8a7a62a0
mangodingo <markakarle@vanderbilt.edu>
2016-08-30 22:39:55 -0500

    scaled the font now and logos revert to home

--- scripts.js
+++ scripts.js
@@ -5,7 +5,9 @@ var revert = function () {
         $("body").css("background-color", "#14322c");
     }
 }
+
 window.onload = function () {
+
     $("#industry").hover(function () {
         $("#large-logo").attr("src","assets/large/gold_160407_Mourningstar_logos_lg_08.png")
         $("body").css("background-color", "#141842");
@@ -39,6 +41,51 @@ window.onload = function () {
         $("#large-logo").attr("src", "assets/large/gold_160407_Mourningstar_logos_lg_10.png")
         $("body").css("background-color", "#44121D");
     })
+    $("#large-logo").click(function () {
+        if (clicked) {
+            clicked = false;
+            revert();
+            $("#large-logo-container").animate({
+                height: '30em',
+                width: '100%'
+            });
+            
+            $('#logo-wrapper').animate({
+                width: '30em'
+            })
+            $("#large-logo").css("width", "30em").animate({
+                height: '30em',
+                width: '30em'
+            }).css("cursor","auto");
+
+            $("#sections").css("position", "").css("top", "100%")
+                .children().show();
+            $(".section-text").hide();
+            $(".section-header").css("text-align", "center")
+
+        };
+    })
+    $("#mourningstar-name").click(function () {
+        if (clicked) {
+            clicked = false;
+            revert();
+            $("#large-logo-container").animate({
+                height: '30em',
+                width: '100%'
+            });
+            $('#logo-wrapper').animate({
+                width: '30em'
+            })
+            $("#large-logo").css("width", "30em").animate({
+                height: '30em',
+                width: '30em'
+            }).css("cursor","auto");
+            $("#sections").css("position", "").animate({ top:'100%' })
+                .children().show();
+            $(".section-text").hide();
+            $(".section-header").css("text-align", "center")
+        };
+    })
     $("#sections").children().click(function () {
         $("#large-logo-container").animate({
             height: '30em',
@@ -50,20 +97,15 @@ window.onload = function () {
         })
         $("#large-logo").css("width","30em").animate({
             height: '15em',
-            width: '15em',
-            left: '0'
-        });
+            width: '15em'
+        }).css("cursor","pointer");
 
         $(this).parent().css("position", "absolute");
-        $(this).parents().animate({
+        $(this).parent().animate({
             top: '0',
-        })
-        $(this).css("text-align", "left")
-        $(this).children().show()
+        });
+        $(this).css("text-align", "left");
+        $(this).children().show();
         $(this).siblings().hide();
-        $(this).removeClass("col-md-2");
-        $(this).children().removeClass("col-md-2")
-        $(this).removeClass("col-md-offset-1")
-        $(this).addClass("col-md-offset-2")
     })
 }
